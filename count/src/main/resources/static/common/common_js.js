51d763a83010fc36759cf18922e38217a89fbe84
a123yaoyao <291778952@qq.com>
2018-10-30 21:38:01 +0800

    删除静态资源

--- count/src/main/resources/static/common/common_js.js
+++
@@ -1,6 +0,0 @@
-document.write('<script src="js/jquery.js"></script> ');
-document.write('<script src="js/common.js"></script> ');
-document.write('<script src="js/core.js"></script> ');
-document.write('<script src="js/jquery.dialog.js"></script> ');
-document.write('<script src="js/index.js"></script> ');
-document.write('<script src="easyui/jquery.easyui.min.js"></script> ');
